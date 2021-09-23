7ddb35d6634ed36c6c8e5c6b6e78f28bcaba2a58
Marcel Kukurowski <marcelow817@gmail.com>
2021-09-23 17:15:34 +0200

    PEP Update

--- Pizza.py
+++ Pizza.py
@@ -3,33 +3,33 @@ import sys
 
 print(sys.argv)
 
-sa1 = (math.pow(((int(sys.argv[3]))/2),2)*math.pi)
+surface_area_1 = math.pow(int(sys.argv[3])/2, 2) * math.pi
 if(len(sys.argv)>5):
-    sa2 = (math.pow(((int(sys.argv[5]))/2),2)*math.pi)
+    surface_area_2 = math.pow(int(sys.argv[5])/2,2) * math.pi
 if(len(sys.argv)>7):    
-    sa3 = (math.pow(((int(sys.argv[7]))/2),2)*math.pi)
+    surface_area_3 = math.pow(int(sys.argv[7])/2,2) * math.pi
 
 # Nazwa pizzeri 1, nazwa pizzy 2, rozmiar pizzy 3, cena pizzy 4
 print(f"Restauracja: {sys.argv[1]}, pizza: {sys.argv[2]}, rozmiar: {sys.argv[3]}cm, cena: {sys.argv[4]}zł")
-if(len(sys.argv)>5):
+if len(sys.argv)>5:
     print(f"Restauracja: {sys.argv[1]}, pizza: {sys.argv[2]}, rozmiar: {sys.argv[5]}cm, cena: {sys.argv[6]}zł")
-if(len(sys.argv)>7):
+if len(sys.argv)>7:
     print(f"Restauracja: {sys.argv[1]}, pizza: {sys.argv[2]}, rozmiar: {sys.argv[7]}cm, cena: {sys.argv[8]}zł")
 
-p1 = float(sys.argv[4])/sa1
+price_1 = float(sys.argv[4])/surface_area_1
 if len(sys.argv)>5 and len(sys.argv)<8:
-    p2 = float(sys.argv[6])/sa2
-    if(p1 > p2):
+    price_2 = float(sys.argv[6])/surface_area_2
+    if price_1 > price_2:
         print(f"Najbardziej opłaca się kupić: {sys.argv[5]}cm")
     else:
         print(f"Najbardziej opłaca się kupić: {sys.argv[3]}cm")
-if(len(sys.argv)>7):
-    p2 = float(sys.argv[6])/sa2
-    p3 = float(sys.argv[8])/sa3
-    price_list = {p1,p2,p3}
-    if(min(price_list) == p1):
+if len(sys.argv)>7:
+    price_2 = float(sys.argv[6])/surface_area_2
+    price_3 = float(sys.argv[8])/surface_area_3
+    price_list = {price_1, price_2, price_3}
+    if min(price_list) == price_1:
         print(f"Najbardziej opłaca się kupić: {sys.argv[3]}cm")
-    elif(min(price_list) == p2):
+    elif min(price_list) == price_2:
         print(f"Najbardziej opłaca się kupić: {sys.argv[5]}cm")
     else:
         print(f"Najbardziej opłaca się kupić: {sys.argv[7]}cm")
